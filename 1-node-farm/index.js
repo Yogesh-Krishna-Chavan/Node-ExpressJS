85a6b4ab0423216668b7c03573f95507ddbd0b73
Yogesh Krishna Chavan <yogeshchavan113@gmail.com>
2024-10-19 00:43:03 +0530

    Creating Api

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,4 @@
-// const fs = require("fs");
+const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
@@ -39,12 +39,24 @@ console.log("will read file...!");
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Server
+
+const data = fs.readFileSync(
+  `${__dirname}/dev-data/data.json`,
+  "utf-8",
+  (err, data) => {
+    const dataObj = JSON.parse(data);
+  }
+);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   if (pathName === "/" || pathName === "/overview") {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
